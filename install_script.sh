7ed479b4564b9d08302c1a26f2be89a8df16138b
Magnus Hansson <hansson.carl.magnus@gmail.com>
2021-09-09 11:44:45 +0200

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -1,1 +1,29 @@
+# Pacman install script for fresh Arch install
+
+# Update system
+pacman -Syu
+
+# Packages for accessing documentation in man and info pages
+pacman -S man-db man-pages texinfo tldr
+
+# System
+pacman -S ranger rsync fzf firefox redshift autocutsel openssh
+
+# Media
+pacman -S zathura cmus vlc feh
+
+# Social
+pacman -S irssi
+
+# LaTeX
+
+# Very necessary
+pacman -S cowsay neofetch
+
+
+
+
+
+
+
 
