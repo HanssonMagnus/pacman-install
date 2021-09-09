e323eda83d8b47ae162edd20678c50237bd24f3b
Magnus Hansson <hansson.carl.magnus@gmail.com>
2021-09-09 23:32:44 +0200

    update

--- install_script.sh
+++ install_script.sh
@@ -7,10 +7,13 @@ pacman -Syu
 pacman -S man-db man-pages texinfo tldr
 
 # System
-pacman -S ranger rsync fzf firefox redshift autocutsel openssh
+pacman -S ranger rsync fzf firefox redshift autocutsel openssh network-manager-applet sysstat acpi
+
+# Audio
+sudo pacman -S pulseaudio pulseaudio-bluetooth pulseaudio-alsa alsa-utils
 
 # Media
-pacman -S zathura cmus vlc feh
+pacman -S zathura cmus vlc feh w3m
 
 # Social
 pacman -S irssi
@@ -20,7 +23,10 @@ pacman -S irssi
 # Very necessary
 pacman -S cowsay neofetch
 
-
+# i3blocks community blocks
+git clone https://github.com/vivien/i3blocks-contrib ~/.config/i3blocks
+cd !$
+cp config.example config
 
 
 
