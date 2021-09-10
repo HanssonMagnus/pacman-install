e97f2bf520b6af8965c2d667973477355e5f9632
Magnus Hansson <hansson.carl.magnus@gmail.com>
2021-09-10 13:32:13 +0200

    install

--- install_script.sh
+++ install_script.sh
@@ -1,32 +1,32 @@
 # Pacman install script for fresh Arch install
 
 # Update system
-pacman -Syu
+sudo pacman -Syu
 
 # Packages for accessing documentation in man and info pages
-pacman -S man-db man-pages texinfo tldr
+sudo pacman -S man-db man-pages texinfo tldr
 
 # System
-pacman -S ranger rsync fzf firefox redshift autocutsel openssh network-manager-applet sysstat acpi
+sudo pacman -S ranger rsync fzf firefox redshift autocutsel openssh network-manager-applet sysstat acpi
 
 # Audio
 sudo pacman -S pulseaudio pulseaudio-bluetooth pulseaudio-alsa alsa-utils
 
 # Media
-pacman -S zathura cmus vlc feh w3m
+sudo pacman -S zathura zathura-djvu zathura-pdf-mupdf cmus vlc feh w3m
 
 # Social
-pacman -S irssi
+sudo pacman -S irssi
 
 # LaTeX
 
 # Very necessary
-pacman -S cowsay neofetch
+sudo pacman -S cowsay neofetch
 
 # i3blocks community blocks
 git clone https://github.com/vivien/i3blocks-contrib ~/.config/i3blocks
-cd !$
-cp config.example config
+#cd !$
+#cp config.example config
 
 
 
