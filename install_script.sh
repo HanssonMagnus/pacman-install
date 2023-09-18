6728b87ff89f2fbbbd378c7695efb1f01672e08e
Magnus Hansson <hansson.carl.magnus@gmail.com>
2023-09-18 16:37:09 +0200

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -3,33 +3,53 @@
 # Update system
 sudo pacman -Syu
 
+# WM
+sudo pacman -S xorg-server xorg-xinit xorg-apps
+sudo pacman -S i3-wm i3status i3blocks demanu
+
+# Terminal and compositor
+sudo pacman -S kitty picom
+
+# Fonts
+#sudo pacman -S noto-fonts noto-fonts-emoji noto-fonts-cjk awesome-terminal-fonts
+sudo pacman -S ttf-roboto-mono-nerd
+sudo pacman -S ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-common ttf-nerd-fonts-symbols-mono
+
 # Packages for accessing documentation in man and info pages
 sudo pacman -S man-db man-pages texinfo tldr
 
 # System
-sudo pacman -S ranger rsync fzf firefox redshift autocutsel openssh network-manager-applet sysstat
-sudo pacman -S acpi pacman-contrib
+sudo pacman -S ranger rsync fzf autocutsel openssh network-manager-applet sysstat
+sudo pacman -S acpi pacman-contrib usbutils
+sudo pacman -S ripgrep fd #for telescope nvim
+
+# Light, brightness, screenshot
+sudo pacman -S redshift light scrot flameshot
 
 # Utilities
-sudo pacman -S tree xdg-user-dirs
+sudo pacman -S tree xdg-user-dirs git base-devel
 
 # Audio
 sudo pacman -S pulseaudio pulseaudio-bluetooth pulseaudio-alsa alsa-utils
 
 # Media
-sudo pacman -S zathura zathura-djvu zathura-pdf-mupdf cmus vlc feh w3m
+sudo pacman -S zathura zathura-djvu zathura-pdf-mupdf cmus vlc feh
 
 # Social
-sudo pacman -S irssi discord signal-desktop
+sudo pacman -S discord signal-desktop
 
 # LaTeX
-sudo pacman -S texlive-most texlive-lang biber
+sudo pacman -S texlive texlive-lang biber
 
 # Laptop (power management)
-sudo pacman -S tlp powertop
+sudo pacman -S tlp acpi_call
 
 # Very necessary
 sudo pacman -S cowsay neofetch
+
+# AUR packages (list all aur packages installed with pacman -Qm)
+yay -S autojump
+yay -S google-chrome
 
 # i3blocks community blocks
 git clone https://github.com/vivien/i3blocks-contrib ~/.config/i3blocks
